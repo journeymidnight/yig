412031855645f73a5e495d3366ae49db1b73066e
陈积 <chenji@le.com>
2016-08-22 10:40:44 +0800

    implement iam module
    Signed-off-by: 陈积 <chenji@le.com>

--- iam/iam.go
+++ iam/iam.go
@@ -3,6 +3,8 @@ package iam
 import (
 	"fmt"
 	"regexp"
+	"github.com/chasex/redis-go-cluster"
+	"time"
 )
 
 // credential container for access and secret keys.
@@ -28,12 +30,30 @@ var IsValidAccessKey = regexp.MustCompile(`^[a-zA-Z0-9\\-\\.\\_\\~]{5,20}$`)
 
 func GetCredential(accessKey string) (credential Credential, err error) {
 	// should use a cache with timeout
-	// TODO
+	// TODO put redis addr to config
+
+	cluster, err := redis.NewCluster(
+		&redis.Options{
+			StartNodes: []string{"127.0.0.1:7000", "127.0.0.1:7001", "127.0.0.1:7002"},
+			ConnTimeout: 50 * time.Millisecond,
+			ReadTimeout: 50 * time.Millisecond,
+			WriteTimeout: 50 * time.Millisecond,
+			KeepAlive: 16,
+			AliveTime: 60 * time.Second,
+		})
+	if err != nil {
+		return Credential{}, fmt.Errorf("connect to redis failed")
+	}
+	reply, err := redis.Strings(cluster.Do("HMGET", accessKey, "secretKey", "uid"))
+	if err != nil {
+		return Credential{}, fmt.Errorf("HMGET failed")
+	}
+
 	return Credential{
-		UserId:          "hehehehe",
-		DisplayName:     "hehehehe",
+		UserId:          reply[1],
+		DisplayName:     reply[1],
 		AccessKeyID:     accessKey,
-		SecretAccessKey: "hehehehe",
+		SecretAccessKey: reply[0],
 	}, nil // For test now
 }
 
@@ -42,7 +62,7 @@ func GetCredentialByUserId(userId string) (credential Credential, err error) {
 	// TODO
 	return Credential{
 		UserId:          userId,
-		DisplayName:     "hehehehe",
+		DisplayName:     userId,
 		AccessKeyID:     "hehehehe",
 		SecretAccessKey: "hehehehe",
 	}, nil // For test now
