2d4fcdc7a7352d69e6e01a9e1a3267457e521e8f
Dongmao Zhang <deanraccoon@gmail.com>
2017-02-17 16:47:53 +0800

    Add range download test case

--- test/config.py
+++ test/config.py
@@ -2,5 +2,5 @@ CONFIG = {
     'access_key': 'hehehehe',
     'secret_key': 'hehehehe',
     'region': 'cn-bj-1',
-    'endpoint': 'http://10.75.144.240:3000'
+    'endpoint': 'http://s3.test.com:3000'
 }
