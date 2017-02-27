da2fedc2e5d6c1aa4af7991ba0f2aba4b06f9d34
Dongmao Zhang <deanraccoon@gmail.com>
2017-02-27 16:16:26 +0800

    Add README for building wrk

--- test/wrk/run.sh
+++ test/wrk/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-echo "RUN luarocks install luaossl FIRST'
-wrk -c64 -d10s --timeout 10s -s put.lua http://yig-test.lecloudapis.com:3000
+echo "RUN luarocks install luaossl FIRST"
+#based on previous test, 3 osd disks could have [1000 ops/s for 4K bytes], [800 ops/s for 127K bytes]
+wrk -c200 -d10s -t 5 --timeout 10s -s put.lua http://yig-test.lecloudapis.com:3000
