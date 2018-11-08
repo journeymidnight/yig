9be6b5a0bf19ddbf4ed41d8aa34c57af19762155
Shaoxiong Li <dahefanteng@gmail.com>
2018-11-08 11:32:43 +0800

    Adjust CI to make it  conviniet for local test

--- integrate/runyig.sh
+++ integrate/runyig.sh
@@ -1,10 +1,11 @@
 BASEDIR=$(dirname $(pwd))
-sudo docker run -d  --name yig \
+sudo docker run -d --rm --name yig \
 			-p 80:80 \
 	                 -p 9000:9000 \
 			 -v ${BASEDIR}/integrate/cephconf:/etc/ceph/ \
 			 -v ${BASEDIR}/integrate/yigconf:/etc/yig/ \
 			 -v ${BASEDIR}:/var/log/yig \
+			 -v ${BASEDIR}:/work \
                          --net=integrate_vpcbr \
                          --ip 10.5.0.18 \
 			 yig /work/build/bin/yig
