7bf351c3664f046a578fd382e2ca0482252ccda0
cxt90730 <cxt90730@126.com>
2018-12-21 18:17:31 +0800

    Fix bug when run delete (#52)

--- integrate/rundelete.sh
+++ integrate/rundelete.sh
@@ -2,5 +2,6 @@ BASEDIR=$(dirname $(pwd))
 sudo docker run -d   --name delete \
 			 -v ${BASEDIR}/integrate/cephconf:/etc/ceph/ \
 			 -v ${BASEDIR}/integrate/yigconf:/etc/yig/ \
-                         --net=integrate_vpcbr \
+			 -v ${BASEDIR}:/work \
+			 --net=integrate_vpcbr \
 			 journeymidnight/yig /work/build/bin/delete
