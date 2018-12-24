feb2a276e4fb688a4891c1e72a10f3a22caf2f23
Dongmao Zhang <deanraccoon@gmail.com>
2018-12-24 20:06:15 +0800

    Add test case for copy-object

--- test/go/object_test.go
+++ test/go/object_test.go
@@ -1,6 +1,8 @@
 package _go
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	. "github.com/journeymidnight/yig/test/go/lib"
 	"net/http"
 	"testing"
@@ -94,6 +96,103 @@ func Test_PreSignedGetObject(t *testing.T) {
 	t.Log("PreSignedGetObject Success.")
 }
 
+func Test_CopyObject(t *testing.T) {
+	//non-cryption
+	svc := NewS3()
+	err := svc.PutObject(TEST_BUCKET, TEST_KEY, TEST_VALUE)
+	if err != nil {
+		t.Fatal("PutObject err:", err)
+	}
+
+	TEST_COPY_KEY := "COPYED:" + TEST_KEY
+	input := &s3.CopyObjectInput{
+		Bucket:     aws.String(TEST_BUCKET),
+		CopySource: aws.String(TEST_BUCKET + "/" + TEST_KEY),
+		Key:        aws.String(TEST_COPY_KEY),
+	}
+	_, err = svc.Client.CopyObject(input)
+	if err != nil {
+		t.Fatal("Copy Object err:", err)
+	}
+
+	//verify them
+	v1, err := svc.GetObject(TEST_BUCKET, TEST_KEY)
+	if err != nil {
+		t.Fatal("Get Object err:", err)
+	}
+	v2, err := svc.GetObject(TEST_BUCKET, TEST_COPY_KEY)
+	if err != nil {
+		t.Fatal("Get Object err:", err)
+	}
+	if v1 != v2 {
+		t.Fatal("Copyed result is not the same.")
+	}
+
+	//clean up
+	svc.DeleteObject(TEST_BUCKET, TEST_KEY)
+	svc.DeleteObject(TEST_BUCKET, TEST_COPY_KEY)
+}
+
+func Test_UpdateObject(t *testing.T) {
+
+	svc := NewS3()
+	err := svc.PutObject(TEST_BUCKET, TEST_KEY, TEST_VALUE)
+	if err != nil {
+		t.Fatal("PutObject err:", err)
+	}
+
+	input := &s3.CopyObjectInput{
+		Bucket:            aws.String(TEST_BUCKET),
+		CopySource:        aws.String(TEST_BUCKET + "/" + TEST_KEY),
+		Key:               aws.String(TEST_KEY),
+		MetadataDirective: aws.String("REPLACE"),
+		CacheControl:      aws.String("max-age:1983"),
+		ContentType:       aws.String("image/jpeg"),
+		Metadata: map[string]*string{
+			"merry":     aws.String("christmas"), //in
+			"happy":     aws.String("new year"),  //in
+			"Christmas": aws.String("EVE"),       //in
+			"hello":     aws.String("world"),     //out
+		},
+	}
+
+	_, err = svc.Client.CopyObject(input)
+	if err != nil {
+		t.Fatal("Copy Object err:", err)
+	}
+	// check the connn
+	params := &s3.HeadObjectInput{
+		Bucket: aws.String(TEST_BUCKET),
+		Key:    aws.String(TEST_KEY),
+	}
+
+	headResult, err := svc.Client.HeadObject(params)
+	if err != nil {
+		t.Fatal("Head object failed")
+	}
+
+	if *headResult.ContentType != "image/jpeg" {
+		t.Fatal("failed to set content type")
+	}
+
+	for k, v := range headResult.Metadata {
+		switch k {
+		case "merry":
+			if *v != "christmas" {
+				t.Fatal("failed to set user defined type")
+			}
+		case "hello":
+			if *v != "world" {
+				t.Fatal("failed to set user defined type")
+			}
+		default:
+			break
+		}
+	}
+
+	svc.DeleteObject(TEST_BUCKET, TEST_KEY)
+}
+
 func Test_Object_End(t *testing.T) {
 	sc := NewS3()
 	err := sc.DeleteObject(TEST_BUCKET, TEST_KEY)
@@ -105,4 +204,5 @@ func Test_Object_End(t *testing.T) {
 		t.Fatal("DeleteBucket err:", err)
 		panic(err)
 	}
+
 }
