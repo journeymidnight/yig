76c1107841a9c2b450cee33c3e7a2955db201520
fireworkmarks <806229693@qq.com>
2019-04-09 21:45:03 +0800

    Get test Func only

--- test/go/specialcharacter_test.go
+++ test/go/specialcharacter_test.go
@@ -6,26 +6,18 @@ import (
 	"testing"
 )
 
-func Test_SpecialCharaterObject_Prepare(t *testing.T) {
+func Test_SpecialCharaterObject(t *testing.T) {
 	sc := NewS3()
 	err := sc.MakeBucket(TEST_BUCKET)
 	if err != nil {
 		t.Fatal("MakeBucket err:", err)
 		panic(err)
 	}
-}
-
-func Test_PutSpecialCharacterObject(t *testing.T) {
-	sc := NewS3()
-	err := sc.PutObject(TEST_BUCKET, TEST_KEY_SPECIAL, TEST_VALUE)
+	err = sc.PutObject(TEST_BUCKET, TEST_KEY_SPECIAL, TEST_VALUE)
 	if err != nil {
 		t.Fatal("PutSpecialCharacterObject err:", err)
 	}
 	t.Log("PutSpecialCharacterObject Success!")
-}
-
-func Test_GetSpecialCharacterObject(t *testing.T) {
-	sc := NewS3()
 	v, err := sc.GetObject(TEST_BUCKET, TEST_KEY_SPECIAL)
 	if err != nil {
 		t.Fatal("GetObject err:", err)
@@ -34,19 +26,11 @@ func Test_GetSpecialCharacterObject(t *testing.T) {
 		t.Fatal("GetSpecialCharacterObject err: value is:", v, ", but should be:", TEST_VALUE)
 	}
 	t.Log("GetSpecialCharacterObject Success value:", v)
-}
-
-func Test_PutSpecialCharacterObjectAcl(t *testing.T) {
-	sc := NewS3()
-	err := sc.PutObjectAcl(TEST_BUCKET, TEST_KEY_SPECIAL, BucketCannedACLPublicRead)
+	err = sc.PutObjectAcl(TEST_BUCKET, TEST_KEY_SPECIAL, BucketCannedACLPublicRead)
 	if err != nil {
 		t.Fatal("PutObjectAcl err:", err)
 	}
 	t.Log("PutObjectAcl Success!")
-}
-
-func Test_GetSpecialCharaterObjectPublicAcl(t *testing.T) {
-	sc := NewS3()
 	url := GenTestSpecialCharaterObjectUrl(sc)
 	statusCode, _, err := HTTPRequestToGetObject(url)
 	if err != nil {
@@ -57,11 +41,7 @@ func Test_GetSpecialCharaterObjectPublicAcl(t *testing.T) {
 		t.Fatal("StatusCode should be STATUS_OK(200), but the code is:", statusCode)
 	}
 	t.Log("GetSpecialCharacterObject With public-read ACL test Success.")
-}
-
-func Test_SpecialCharaterObject_End(t *testing.T) {
-	sc := NewS3()
-	err := sc.DeleteObject(TEST_BUCKET, TEST_KEY_SPECIAL)
+	err = sc.DeleteObject(TEST_BUCKET, TEST_KEY_SPECIAL)
 	if err != nil {
 		t.Log("DeleteSpecialCharacterObject err:", err)
 	}
@@ -70,5 +50,4 @@ func Test_SpecialCharaterObject_End(t *testing.T) {
 		t.Fatal("DeleteBucket err:", err)
 		panic(err)
 	}
-
 }
