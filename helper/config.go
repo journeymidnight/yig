5db4d4cb14e768924496725629bb30b8c2b0be08
YuZou <hopkings2005@hotmail.com>
2019-05-20 15:34:27 +0800

    replace the redigo with go_redis.

--- helper/config.go
+++ helper/config.go
@@ -11,61 +11,64 @@ const (
 )
 
 type Config struct {
-	S3Domain                   []string `toml:"s3domain"`     // Domain name of YIG
-	Region                     string `toml:"region"`       // Region name this instance belongs to, e.g cn-bj-1
-	IamEndpoint                string `toml:"iam_endpoint"` // le IAM endpoint address
-	IamKey                     string `toml:"iam_key"`
-	IamSecret                  string `toml:"iam_secret"`
-	IamVersion                 string `toml:"iam_version"`
-	LogPath                    string `toml:"log_path"`
-	AccessLogPath              string `toml:"access_log_path"`
-	AccessLogFormat            string `toml:"access_log_format"`
-	PanicLogPath               string `toml:"panic_log_path"`
-	PidFile                    string `toml:"pid_file"`
-	BindApiAddress             string `toml:"api_listener"`
-	BindAdminAddress           string `toml:"admin_listener"`
-	SSLKeyPath                 string `toml:"ssl_key_path"`
-	SSLCertPath                string `toml:"ssl_cert_path"`
-	ZookeeperAddress           string `toml:"zk_address"`
+	S3Domain         []string `toml:"s3domain"`     // Domain name of YIG
+	Region           string   `toml:"region"`       // Region name this instance belongs to, e.g cn-bj-1
+	IamEndpoint      string   `toml:"iam_endpoint"` // le IAM endpoint address
+	IamKey           string   `toml:"iam_key"`
+	IamSecret        string   `toml:"iam_secret"`
+	IamVersion       string   `toml:"iam_version"`
+	LogPath          string   `toml:"log_path"`
+	AccessLogPath    string   `toml:"access_log_path"`
+	AccessLogFormat  string   `toml:"access_log_format"`
+	PanicLogPath     string   `toml:"panic_log_path"`
+	PidFile          string   `toml:"pid_file"`
+	BindApiAddress   string   `toml:"api_listener"`
+	BindAdminAddress string   `toml:"admin_listener"`
+	SSLKeyPath       string   `toml:"ssl_key_path"`
+	SSLCertPath      string   `toml:"ssl_cert_path"`
+	ZookeeperAddress string   `toml:"zk_address"`
 
-	InstanceId                 string // if empty, generated one at server startup
-	ConcurrentRequestLimit     int
-	HbaseZnodeParent           string        // won't change default("/hbase") if leave this option empty
-	HbaseTimeout               time.Duration // in seconds
-	DebugMode                  bool          `toml:"debug_mode"`
-	AdminKey                   string        `toml:"admin_key"` //used for tools/admin to communicate with yig
-	GcThread                   int    `toml:"gc_thread"`
-	LcThread                   int    //used for tools/lc only, set worker numbers to do lc
-	LcDebug                    bool   //used for tools/lc only, if this was set true, will treat days as seconds
-	LogLevel                   int    `toml:"log_level"` //1-20
-	CephConfigPattern          string `toml:"ceph_config_pattern"`
-	ReservedOrigins            string `toml:"reserved_origins"`// www.ccc.com,www.bbb.com,127.0.0.1
-	MetaStore                  string `toml:"meta_store"`
-	TidbInfo                   string `toml:"tidb_info"`
-	KeepAlive                  bool   `toml:"keepalive"`
+	InstanceId             string // if empty, generated one at server startup
+	ConcurrentRequestLimit int
+	HbaseZnodeParent       string        // won't change default("/hbase") if leave this option empty
+	HbaseTimeout           time.Duration // in seconds
+	DebugMode              bool          `toml:"debug_mode"`
+	AdminKey               string        `toml:"admin_key"` //used for tools/admin to communicate with yig
+	GcThread               int           `toml:"gc_thread"`
+	LcThread               int           //used for tools/lc only, set worker numbers to do lc
+	LcDebug                bool          //used for tools/lc only, if this was set true, will treat days as seconds
+	LogLevel               int           `toml:"log_level"` //1-20
+	CephConfigPattern      string        `toml:"ceph_config_pattern"`
+	ReservedOrigins        string        `toml:"reserved_origins"` // www.ccc.com,www.bbb.com,127.0.0.1
+	MetaStore              string        `toml:"meta_store"`
+	TidbInfo               string        `toml:"tidb_info"`
+	KeepAlive              bool          `toml:"keepalive"`
 
 	//About cache
-	RedisAddress                   string `toml:"redis_address"`           // redis connection string, e.g localhost:1234
-	RedisConnectionNumber          int    `toml:"redis_connection_number"` // number of connections to redis(i.e max concurrent request number)
-	RedisPassword                  string `toml:"redis_password"`	                             // redis auth password
-	MetaCacheType                  int    `toml:"meta_cache_type"`
-	EnableDataCache                bool   `toml:"enable_data_cache"`
-	RedisConnectTimeout            int    `toml:"redis_connect_timeout"`
-	RedisReadTimeout               int    `toml:"redis_read_timeout"`
-	RedisWriteTimeout              int    `toml:"redis_write_timeout"`
-	RedisKeepAlive                 int    `toml:"redis_keepalive"`
-	RedisPoolMaxIdle               int    `toml:"redis_pool_max_idle"`
-	RedisPoolIdleTimeout           int    `toml:"redis_pool_idle_timeout"`
+	RedisAddress            string   `toml:"redis_address"`           // redis connection string, e.g localhost:1234
+	RedisConnectionNumber   int      `toml:"redis_connection_number"` // number of connections to redis(i.e max concurrent request number)
+	RedisPassword           string   `toml:"redis_password"`          // redis auth password
+	MetaCacheType           int      `toml:"meta_cache_type"`
+	EnableDataCache         bool     `toml:"enable_data_cache"`
+	RedisMode               int      `toml:"redis_mode"`
+	RedisNodes              []string `toml:"redis_nodes"`
+	RedisSentinelMasterName string   `toml:"redis_sentinel_master_name"`
+	RedisConnectTimeout     int      `toml:"redis_connect_timeout"`
+	RedisReadTimeout        int      `toml:"redis_read_timeout"`
+	RedisWriteTimeout       int      `toml:"redis_write_timeout"`
+	RedisKeepAlive          int      `toml:"redis_keepalive"`
+	RedisPoolMaxIdle        int      `toml:"redis_pool_max_idle"`
+	RedisPoolIdleTimeout    int      `toml:"redis_pool_idle_timeout"`
 
 	// If the value is not 0, the cached ping detection will be turned on, and the interval is the number of seconds.
-	CacheCircuitCheckInterval      int    `toml:"cache_circuit_check_interval"`
+	CacheCircuitCheckInterval int `toml:"cache_circuit_check_interval"`
 	// This property sets the amount of seconds, after tripping the circuit,
 	// to reject requests before allowing attempts again to determine if the circuit should again be closed.
-	CacheCircuitCloseSleepWindow   int    `toml:"cache_circuit_close_sleep_window"`
+	CacheCircuitCloseSleepWindow int `toml:"cache_circuit_close_sleep_window"`
 	// This value is how may consecutive passing requests are required before the circuit is closed
-	CacheCircuitCloseRequiredCount int    `toml:"cache_circuit_close_required_count"`
+	CacheCircuitCloseRequiredCount int `toml:"cache_circuit_close_required_count"`
 	// This property sets the minimum number of requests in a rolling window that will trip the circuit.
-	CacheCircuitOpenThreshold      int    `toml:"cache_circuit_open_threshold"`
+	CacheCircuitOpenThreshold int `toml:"cache_circuit_open_threshold"`
 
 	KMS KMSConfig `toml:"kms"`
 }
@@ -148,7 +151,7 @@ func MarshalTOMLConfig() error {
 	CONFIG.RedisWriteTimeout = Ternary(c.RedisWriteTimeout < 0, 0, c.RedisWriteTimeout).(int)
 	CONFIG.RedisKeepAlive = Ternary(c.RedisKeepAlive < 0, 0, c.RedisKeepAlive).(int)
 	CONFIG.RedisPoolMaxIdle = Ternary(c.RedisPoolMaxIdle < 0, 0, c.RedisPoolMaxIdle).(int)
-	CONFIG.RedisPoolIdleTimeout = Ternary(c.RedisPoolIdleTimeout < 0 , 0, c.RedisPoolIdleTimeout).(int)
+	CONFIG.RedisPoolIdleTimeout = Ternary(c.RedisPoolIdleTimeout < 0, 0, c.RedisPoolIdleTimeout).(int)
 
 	CONFIG.CacheCircuitCheckInterval = Ternary(c.CacheCircuitCheckInterval < 0, 0, c.CacheCircuitCheckInterval).(int)
 	CONFIG.CacheCircuitCloseSleepWindow = Ternary(c.CacheCircuitCloseSleepWindow < 0, 0, c.CacheCircuitCloseSleepWindow).(int)
---
+++ redis/redis_client.go
@@ -0,0 +1,147 @@
+package redis
+
+import (
+	"errors"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/journeymidnight/yig/helper"
+)
+
+const (
+	REDIS_CLIENT_UNKNOWN = iota
+	REDIS_NORMAL_CLIENT
+	REDIS_CLUSTER_CLIENT
+	REDIS_SENTINEL_CLIENT
+)
+
+const ERR_NOT_INIT_MSG = "redis client is not initialized yet."
+
+type RedisCli struct {
+	clientType         int
+	redisClient        *redis.Client
+	redisClusterClient *redis.ClusterClient
+}
+
+func NewRedisCli() *RedisCli {
+	return &RedisCli{
+		clientType: REDIS_CLIENT_UNKNOWN,
+	}
+}
+
+func (cli *RedisCli) Init() {
+	switch helper.CONFIG.RedisMode {
+	case 1:
+		options := &redis.ClusterOptions{
+			ReadTimeout:  time.Duration(helper.CONFIG.RedisReadTimeout) * time.Second,
+			DialTimeout:  time.Duration(helper.CONFIG.RedisConnectTimeout) * time.Second,
+			WriteTimeout: time.Duration(helper.CONFIG.RedisWriteTimeout) * time.Second,
+			IdleTimeout:  time.Duration(helper.CONFIG.RedisKeepAlive) * time.Second,
+		}
+		if helper.CONFIG.RedisPassword != "" {
+			options.Password = helper.CONFIG.RedisPassword
+		}
+		cli.redisClusterClient = redis.NewClusterClient(options)
+		cli.clientType = REDIS_CLUSTER_CLIENT
+	case 2:
+		options := &redis.FailoverOptions{
+			MasterName:    helper.CONFIG.RedisSentinelMasterName,
+			SentinelAddrs: helper.CONFIG.RedisNodes,
+			ReadTimeout:   time.Duration(helper.CONFIG.RedisReadTimeout) * time.Second,
+			DialTimeout:   time.Duration(helper.CONFIG.RedisConnectTimeout) * time.Second,
+			WriteTimeout:  time.Duration(helper.CONFIG.RedisWriteTimeout) * time.Second,
+			IdleTimeout:   time.Duration(helper.CONFIG.RedisKeepAlive) * time.Second,
+		}
+		if helper.CONFIG.RedisPassword != "" {
+			options.Password = helper.CONFIG.RedisPassword
+		}
+		cli.redisClient = redis.NewFailoverClient(options)
+		cli.clientType = REDIS_SENTINEL_CLIENT
+	default:
+		options := &redis.Options{
+			ReadTimeout:  time.Duration(helper.CONFIG.RedisReadTimeout) * time.Second,
+			DialTimeout:  time.Duration(helper.CONFIG.RedisConnectTimeout) * time.Second,
+			WriteTimeout: time.Duration(helper.CONFIG.RedisWriteTimeout) * time.Second,
+			IdleTimeout:  time.Duration(helper.CONFIG.RedisKeepAlive) * time.Second,
+		}
+
+		if helper.CONFIG.RedisPassword != "" {
+			options.Password = helper.CONFIG.RedisPassword
+		}
+
+		cli.redisClient = redis.NewClient(options)
+		cli.clientType = REDIS_NORMAL_CLIENT
+	}
+}
+
+func (cli *RedisCli) Close() error {
+	switch cli.clientType {
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.Close()
+	case REDIS_NORMAL_CLIENT, REDIS_SENTINEL_CLIENT:
+		return cli.redisClient.Close()
+	default:
+		return nil
+	}
+}
+
+func (cli *RedisCli) Del(key string) (int64, error) {
+	switch cli.clientType {
+	case REDIS_NORMAL_CLIENT, REDIS_SENTINEL_CLIENT:
+		return cli.redisClient.Del(key).Result()
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.Del(key).Result()
+	default:
+		return 0, errors.New(ERR_NOT_INIT_MSG)
+	}
+}
+
+/*
+* @key: input key
+* @value: input value
+* @expire: expiration for the key in milliseconds.
+ */
+
+func (cli *RedisCli) Set(key string, value interface{}, expire int64) (string, error) {
+	switch cli.clientType {
+	case REDIS_NORMAL_CLIENT, REDIS_SENTINEL_CLIENT:
+		return cli.redisClient.Set(key, value, time.Duration(expire)*time.Millisecond).Result()
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.Set(key, value, time.Duration(expire)*time.Millisecond).Result()
+	default:
+		return "", errors.New(ERR_NOT_INIT_MSG)
+	}
+}
+
+func (cli *RedisCli) Get(key string) ([]byte, error) {
+	switch cli.clientType {
+	case REDIS_NORMAL_CLIENT:
+		return cli.redisClient.Get(key).Bytes()
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.Get(key).Bytes()
+	default:
+		return nil, errors.New(ERR_NOT_INIT_MSG)
+	}
+}
+
+func (cli *RedisCli) GetRange(key string, start, end int64) ([]byte, error) {
+	switch cli.clientType {
+	case REDIS_NORMAL_CLIENT, REDIS_SENTINEL_CLIENT:
+		return cli.redisClient.GetRange(key, start, end).Bytes()
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.GetRange(key, start, end).Bytes()
+	default:
+		return nil, errors.New(ERR_NOT_INIT_MSG)
+	}
+}
+
+func (cli *RedisCli) Publish(channel string, message interface{}) (int64, error) {
+	switch cli.clientType {
+	case REDIS_NORMAL_CLIENT, REDIS_SENTINEL_CLIENT:
+		return cli.redisClient.Publish(channel, message).Result()
+	case REDIS_CLUSTER_CLIENT:
+		return cli.redisClusterClient.Publish(channel, message).Result()
+	default:
+		return 0, errors.New(ERR_NOT_INIT_MSG)
+	}
+}
