b2132272828f14d83fbdd4913275f101a893cf88
YuZou <hopkings2005@hotmail.com>
2019-05-30 10:40:34 +0800

    add ci tests for bucket usage update.

---
+++ test/ci/bucket_usage_test.go
@@ -0,0 +1,29 @@
+package ci
+
+import (
+	. "github.com/journeymidnight/yig/test/go/lib"
+	. "gopkg.in/check.v1"
+)
+
+func (cs *CISuite) TestBucketUsage(c *C) {
+	bn := "buckettest"
+	key := "objt1"
+	sc := NewS3()
+	err := sc.MakeBucket(bn)
+	c.Assert(err, Equals, nil)
+	defer sc.DeleteBucket(bn)
+	b, err := cs.storage.GetBucket(bn)
+	c.Assert(err, Equals, nil)
+	c.Assert(b, Not(Equals), nil)
+	c.Assert(b.Usage == 0, Equals, true)
+	randUtil := &RandUtil{}
+	val := randUtil.RandString(128 << 10)
+	err = sc.PutObject(bn, key, val)
+	c.Assert(err, Equals, nil)
+	defer sc.DeleteObject(bn, key)
+	// check the bucket usage.
+	b, err = cs.storage.GetBucket(bn)
+	c.Assert(err, Equals, nil)
+	c.Assert(b, Not(Equals), nil)
+	c.Assert(b.Usage, Equals, int64(128<<10))
+}
