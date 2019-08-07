00a8b40a954c744b8928eb295489b2b714f1e743
cuixiaotian <cuixiaotian@unicloud.com>
2019-08-07 14:47:06 +0800

    try proxy

--- integrate/buildyig.sh
+++ integrate/buildyig.sh
@@ -1,2 +1,2 @@
 BASEDIR=$(dirname $(pwd))
-sudo docker run --rm -v ${BASEDIR}:/work -w /work journeymidnight/yig bash -c 'make build_internal'
+sudo docker run --rm -v ${BASEDIR}:/work -w /work journeymidnight/yig bash -c 'export GOPROXY=https://goproxy.cn;make build_internal'
