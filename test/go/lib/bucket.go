b0a9860e80b7d9f8f8776638eb96b683a2536273
fireworkmarks <806229693@qq.com>
2020-01-16 09:38:20 +0800

    Added test for bugs in ListObjects

--- test/go/lib/bucket.go
+++ test/go/lib/bucket.go
@@ -1,8 +1,8 @@
 package lib
 
 import (
-	"github.com/journeymidnight/aws-sdk-go/aws"
-	"github.com/journeymidnight/aws-sdk-go/service/s3"
+"github.com/journeymidnight/aws-sdk-go/aws"
+"github.com/journeymidnight/aws-sdk-go/service/s3"
 )
 
 func (s3client *S3Client) MakeBucket(bucketName string) (err error) {
@@ -31,6 +31,20 @@ func (s3client *S3Client) HeadBucket(bucketName string) (err error) {
 	}
 	if _, err = s3client.Client.HeadBucket(params); err != nil {
 		return err
+	}
+	return
+}
+
+func (s3client *S3Client) ListObjects(bucketName string) (objects []*s3.Object, err error){
+	params := &s3.ListObjectsInput{
+		Bucket:  aws.String(bucketName),
+	}
+	result, err := s3client.Client.ListObjects(params)
+	if err != nil {
+		return
+	}
+	if result != nil {
+		objects = result.Contents
 	}
 	return
 }
