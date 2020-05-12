0d5e26f6a4b80d03d8e56fbb1dbe05f1d6e3e5c2
cuixiaotian <cuixiaotian@unicloud.com>
2020-05-12 16:12:04 +0800

    save code

--- meta/client/tikvclient/lifecycle.go
+++ meta/client/tikvclient/lifecycle.go
@@ -1,17 +1,38 @@
 package tikvclient
 
 import (
+	. "github.com/journeymidnight/yig/error"
 	. "github.com/journeymidnight/yig/meta/types"
 )
 
-func genLifecycleKey() []byte {
-	return GenKey(TableLifeCyclePrefix)
+// **Key**: {BucketName}\{ObjectName}
+func genLifecycleKey(bucketName string) []byte {
+	return GenKey(TableLifeCyclePrefix, bucketName)
 }
 
 //lc
-func (c *TiKVClient) PutBucketToLifeCycle(lifeCycle LifeCycle) error       { return nil }
-func (c *TiKVClient) GetBucketLifeCycle(bucket Bucket) (*LifeCycle, error) { return nil, nil }
-func (c *TiKVClient) RemoveBucketFromLifeCycle(bucket Bucket) error        { return nil }
+func (c *TiKVClient) PutBucketToLifeCycle(lifeCycle LifeCycle) error {
+
+	return nil
+}
+
+func (c *TiKVClient) GetBucketLifeCycle(bucket Bucket) (*LifeCycle, error) {
+	key := genLifecycleKey(bucket.Name)
+	var lc LifeCycle
+	ok, err := c.TxGet(key, &lc)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return nil, ErrNoSuchKey
+	}
+	return &lc, nil
+}
+
+func (c *TiKVClient) RemoveBucketFromLifeCycle(bucket Bucket) error {
+	return nil
+}
+
 func (c *TiKVClient) ScanLifeCycle(limit int, marker string) (result ScanLifeCycleResult, err error) {
 	return
 }
