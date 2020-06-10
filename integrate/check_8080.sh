0efd07e88914b3801d87a1c01cd947304b41ee32
fireworkmarks <806229693@qq.com>
2020-06-10 15:08:47 +0800

    Exception handling

--- integrate/check_8080.sh
+++ integrate/check_8080.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+a=`lsof -i:8080 | wc -l`
 if [ $a -eq 0 ];then
     echo "No concurrent build pipeline, you can start building"
-    sleep 10
+    random=$(( (RANDOM % 60) + 1 ))
+    echo -n "$random"
+    sleep $random
 else
     echo "There are already pipelines under construction, wait for 20s to check again until the other pipelines are completed"
     sleep 20
 fi
 for ((i=1;i<2;))
 do
-a=`lsof -i:8080 | wc -l`
 if [ $a -eq 0 ];then
     echo "No concurrent build pipeline, you can start building"
     i=$i+1
