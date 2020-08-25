403f57d50be5fd2d121423a45d45359356c27aeb
cxt90730 <cxt90730@126.com>
2020-08-25 17:20:01 +0800

    add repair usage

--- tools/tikvtool/funcs.go
+++ tools/tikvtool/funcs.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/journeymidnight/yig/meta/common"
 
 	"github.com/journeymidnight/yig/meta/types"
 
@@ -98,8 +101,7 @@ func GetFunc(key string) error {
 			} else {
 				k = tikvclient.GenKey(global.Bucket, key, global.Version)
 			}
-			printKey := bytes.Replace(k, []byte{0x31}, []byte("\\"), -1)
-			fmt.Println("key:", string(printKey))
+			fmt.Println("key:", string(DecodeKey(k)))
 			var o types.Object
 			ok, err := c.TxGet(k, &o, nil)
 			if err != nil {
@@ -253,4 +255,186 @@ func DropFunc() error {
 	}
 	fmt.Println("Delete key count:", count)
 	return nil
+}
+
+func RepairFunc(hourKey string) (err error) {
+	if global.Bucket == "" {
+		fmt.Println("You must specified a bucket.")
+		return nil
+	}
+
+	repairTime, err := time.ParseInLocation("2006010215", hourKey, time.Local)
+	if err != nil {
+		return
+	}
+	repairTs := repairTime.UnixNano()
+	c := tikvclient.NewClient(strings.Split(global.PDs, ","))
+	fmt.Println("====== Start Repair Bucket :", global.Bucket, "======")
+	fmt.Println("Repair Time:", repairTime)
+	fmt.Println("Repair TimeStamp:", repairTs)
+	startKey := tikvclient.GenKey(global.Bucket, tikvclient.TableMinKeySuffix)
+	endKey := tikvclient.GenKey(global.Bucket, tikvclient.TableMaxKeySuffix)
+
+	tx, err := c.TxnCli.Begin(context.TODO())
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err == nil {
+			err = tx.Commit(context.Background())
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	fmt.Println("Check bucket...")
+	bucketKey := tikvclient.GenKey(tikvclient.TableBucketPrefix, global.Bucket)
+	var b types.Bucket
+	ok, err := c.TxGet(bucketKey, &b, tx)
+	if err != nil {
+		return
+	}
+	if !ok {
+		fmt.Println("No such bucket:", global.Bucket)
+		return
+	}
+	fmt.Println("Check bucket ok!")
+	it, err := tx.Iter(context.TODO(), startKey, endKey)
+	if err != nil {
+		return err
+	}
+	defer it.Close()
+
+	var o_standard, o_standardIa, o_glacier int64
+	fmt.Println("Scan object...")
+
+	// scan object
+	for it.Valid() {
+		k, v := it.Key(), it.Value()
+		var o types.Object
+		err := helper.MsgPackUnMarshal(v, &o)
+		printKey := string(EncodeKey(k))
+		if err != nil {
+			fmt.Println("MsgPackUnMarshal err:", err, "key:", printKey)
+			break
+		}
+		if o.CreateTime > uint64(repairTs) {
+			if err := it.Next(context.TODO()); err != nil && it.Valid() {
+				return err
+			}
+			continue
+		}
+		fmt.Println("Key:", printKey, "StorageClass:", o.StorageClass.ToString(), "Size:", o.Size, "CTime:", o.CreateTime)
+		switch o.StorageClass {
+		case common.ObjectStorageClassStandard:
+			o_standard += o.Size
+		case common.ObjectStorageClassStandardIa:
+			o_standardIa += common.CorrectDeltaSize(o.StorageClass, o.Size)
+		case common.ObjectStorageClassGlacier:
+			o_glacier += common.CorrectDeltaSize(o.StorageClass, o.Size)
+		}
+
+		if err := it.Next(context.TODO()); err != nil && it.Valid() {
+			return err
+		}
+	}
+	fmt.Println("Scan object result: Standard:", o_standard, "StandardIA:", o_standardIa, "Glacier:", o_glacier)
+
+	var p_standard, p_standardIa, p_glacier int64
+	startKey = tikvclient.GenKey(tikvclient.TableMultipartPrefix, global.Bucket, tikvclient.TableMinKeySuffix)
+	endKey = tikvclient.GenKey(tikvclient.TableMultipartPrefix, global.Bucket, tikvclient.TableMaxKeySuffix)
+	fmt.Println("Scan multipart...")
+	it, err = tx.Iter(context.TODO(), startKey, endKey)
+	if err != nil {
+		return
+	}
+
+	// scan multipart
+	for it.Valid() {
+		k, v := it.Key(), it.Value()
+		var m types.Multipart
+		err := helper.MsgPackUnMarshal(v, &m)
+		printKey := string(EncodeKey(k))
+		if err != nil {
+			fmt.Println("MsgPackUnMarshal err:", err, "key:", printKey)
+			break
+		}
+		startPartKey := tikvclient.GenKey(tikvclient.TableObjectPartPrefix, global.Bucket, m.ObjectName, tikvclient.TableMinKeySuffix)
+		endPartKey := tikvclient.GenKey(tikvclient.TableObjectPartPrefix, global.Bucket, m.ObjectName, tikvclient.TableMaxKeySuffix)
+		it2, err := tx.Iter(context.TODO(), startPartKey, endPartKey)
+		if err != nil {
+			return err
+		}
+		for it2.Valid() {
+			k2, v2 := it2.Key(), it2.Value()
+			var p types.Part
+			err := helper.MsgPackUnMarshal(v2, &p)
+			printKey := string(EncodeKey(k2))
+			if err != nil {
+				fmt.Println("MsgPackUnMarshal err:", err, "key:", printKey)
+				break
+			}
+
+			partCTime, err := time.ParseInLocation(types.CREATE_TIME_LAYOUT, p.LastModified, time.Local)
+			if err != nil {
+				return err
+			}
+			partCTs := partCTime.UnixNano()
+			if partCTs > repairTs {
+				if err := it2.Next(context.TODO()); err != nil && it2.Valid() {
+					return err
+				}
+				continue
+			}
+
+			fmt.Println("Key:", printKey, "StorageClass:", m.Metadata.StorageClass.ToString(), "PartNumber:", p.PartNumber, "Size:", p.Size, "CTime:", partCTs)
+			switch m.Metadata.StorageClass {
+			case common.ObjectStorageClassStandard:
+				p_standard += p.Size
+			case common.ObjectStorageClassStandardIa:
+				p_standardIa += common.CorrectDeltaSize(m.Metadata.StorageClass, p.Size)
+			case common.ObjectStorageClassGlacier:
+				p_glacier += common.CorrectDeltaSize(m.Metadata.StorageClass, p.Size)
+			}
+			if err := it2.Next(context.TODO()); err != nil && it2.Valid() {
+				return err
+			}
+		}
+
+		if err := it.Next(context.TODO()); err != nil && it.Valid() {
+			return err
+		}
+	}
+
+	fmt.Println("Scan multipart result: Standard:", p_standard, "StandardIA:", p_standardIa, "Glacier:", p_glacier)
+	standard := o_standard + p_standard
+	standardIa := o_standardIa + p_standardIa
+	glacier := o_glacier + p_glacier
+	fmt.Println("Sum result of bucket: ", b.Name, "Standard:", standard, "StandardIA:", standardIa, "Glacier:", glacier)
+
+	// Update TiKV
+	if !global.Verbose {
+		key := genUserBucketKey(b.OwnerId, b.Name)
+		val := tikvclient.BucketUsage{
+			Standard:   standard,
+			StandardIa: standardIa,
+			Glacier:    glacier,
+		}
+		v, err := helper.MsgPackMarshal(val)
+		if err != nil {
+			return err
+		}
+		err = tx.Set(key, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("Finished")
+	return
+}
+
+func genUserBucketKey(ownerId, bucketName string) []byte {
+	return tikvclient.GenKey(tikvclient.TableUserBucketPrefix, ownerId, bucketName)
 }
--- tools/tikvtool/main.go
+++ tools/tikvtool/main.go
@@ -22,10 +22,10 @@ import (
 
 var table, startKey, endKey string
 var maxKeys int
-var verbose bool
 
 type GlobalOption struct {
 	PDs          string
+	Verbose      bool
 	IsKeyBytes   bool
 	IsValueBytes bool
 	IsMsgPack    bool
@@ -55,6 +55,11 @@ func main() {
 			Value:       "pd1:2378, pd2:2377",
 			Usage:       "One or a set of pd addresses. e.g: pd1:2379 or pd1:2379,pd2:2378,pd3:2377",
 			Destination: &global.PDs,
+		},
+		cli.BoolFlag{
+			Name:        "verbose, z",
+			Usage:       "verbose option",
+			Destination: &global.Verbose,
 		},
 		cli.BoolFlag{
 			Name:        "keybytes",
@@ -172,15 +177,17 @@ func main() {
 			Action: func(c *cli.Context) error {
 				return DropFunc()
 			},
-			ArgsUsage: "[--verbose|-v]",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:        "verbose,v",
-					Required:    false,
-					Usage:       "print deleted key",
-					Destination: &verbose,
-				},
+		},
+		{
+			Name:  "repair",
+			Usage: "repair the usage of specified bucket",
+			Action: func(c *cli.Context) error {
+				if len(c.Args()) != 1 {
+					return errors.New("Invalid arguments.")
+				}
+				return RepairFunc(c.Args()[0])
 			},
+			ArgsUsage: "Repair the data before this hour. e.g. 2020082012",
 		},
 	}
 
