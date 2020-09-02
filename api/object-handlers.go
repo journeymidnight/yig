2c8987913721c59c97ee77587fccb9c0eda05a2d
mafuda00774 <mafuda@unicloud.com>
2020-09-02 14:08:20 +0800

    Fix the situation where headObject returns abnormally

--- api/object-handlers.go
+++ api/object-handlers.go
@@ -175,12 +175,19 @@ func (o *GetObjectResponseWriter) Write(p []byte) (int, error) {
 		}
 		// Set headers on the first write.
 		// Set standard object headers.
-		SetObjectHeaders(o.w, o.object, o.hrange, o.statusCode)
+		SetObjectHeaders(o.w, o.object)
 		// Set any additional requested response headers.
 		// You must sign the request, either using an Authorization header or a presigned URL, when using these parameters.
 		// They cannot be used with an unsigned (anonymous) request.
 		if o.authType > signature.AuthTypeAnonymous && o.statusCode == http.StatusOK {
 			setGetRespHeaders(o.w, o.r.URL.Query())
+		}
+		// for providing ranged content
+		if o.hrange != nil && o.hrange.OffsetBegin > -1 {
+			// Override content-length
+			o.w.Header().Set("Content-Length", strconv.FormatInt(o.hrange.GetLength(), 10))
+			o.w.Header().Set("Content-Range", o.hrange.String())
+			o.statusCode = http.StatusPartialContent
 		}
 		o.w.WriteHeader(o.statusCode)
 		o.dataWritten = true
@@ -231,8 +238,7 @@ func (api ObjectAPIHandlers) GetObjectHandler(w http.ResponseWriter, r *http.Req
 			WriteErrorResponse(w, r, ErrMethodNotAllowed)
 			return
 		}
-		SetObjectHeaders(w, object, nil, http.StatusNotFound)
-		w.WriteHeader(http.StatusNotFound)
+		SetObjectHeaders(w, object)
 		WriteErrorResponse(w, r, ErrNoSuchKey)
 		return
 	}
@@ -413,8 +419,7 @@ func (api ObjectAPIHandlers) HeadObjectHandler(w http.ResponseWriter, r *http.Re
 			WriteErrorResponse(w, r, ErrMethodNotAllowed)
 			return
 		}
-		SetObjectHeaders(w, object, nil, http.StatusNotFound)
-		w.WriteHeader(http.StatusNotFound)
+		SetObjectHeaders(w, object)
 		WriteErrorResponse(w, r, ErrNoSuchKey)
 		return
 	}
@@ -476,7 +481,7 @@ func (api ObjectAPIHandlers) HeadObjectHandler(w http.ResponseWriter, r *http.Re
 
 	// Successful response.
 	// Set standard object headers.
-	SetObjectHeaders(w, object, nil, http.StatusOK)
+	SetObjectHeaders(w, object)
 	w.WriteHeader(http.StatusOK)
 }
 
