91717715c0cc5604fc854e7e17134eae859eab1c
mafuda00774 <mafuda@unicloud.com>
2020-09-15 15:27:45 +0800

    Fix the problem that the installation of the package displays abnormal errors

--- integrate/runlc.sh
+++ integrate/runlc.sh
@@ -1,9 +1,9 @@
 BASEDIR=$(dirname $(pwd))
 echo ${BASEDIR}
 WORKDIR=$1
-sudo docker rm --force lc
-if [ -x "$BASEDIR/lc" ]; then 
-    sudo docker run -d --name lc \
+sudo docker rm --force yig-lc
+if [ -x "$BASEDIR/yig-lc" ]; then
+    sudo docker run -d --name yig-lc \
 			 -v ${BASEDIR}/integrate/timezone:/etc/localtime:ro \
 			 -v ${BASEDIR}/integrate/cephconf:/etc/ceph/ \
 			 -v ${BASEDIR}/integrate/yigconf:/etc/yig/ \
@@ -11,6 +11,6 @@ if [ -x "$BASEDIR/lc" ]; then
 			 -v ${BASEDIR}:${WORKDIR} \
                          --net=integrate_vpcbr \
                          --ip 10.5.0.20 \
-			 journeymidnight/yig /work/lc
+			 journeymidnight/yig /work/yig-lc
     echo "started lc from local dir"
 fi
